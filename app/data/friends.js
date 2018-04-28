b8b07bf16e5c295834184475f18610d271c2c9df
Evan Morrison <evan@Evans-MacBook-Pro.local>
2018-04-27 23:52:36 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friendsData = [
+    {
+      name: "Cool dude",
+      photo: "https://thumbs.dreamstime.com/z/old-man-celebrating-laptop-16618166.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+
+    {
+        name: "Sasha",
+        photo: "https://i.imgflip.com/1wh3bz.jpg",
+        scores: [4, 5, 1, 3, 4, 1, 4, 5, 5, 5]
+      },
+
+      {
+        name: "Excitable Old Man",
+        photo: "https://thumbs.dreamstime.com/z/older-man-giving-thumb-up-computer-game-16618158.jpg",
+        scores: [4, 5, 1, 5, 4, 1, 2, 2, 5, 3]
+      },
+
+    
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
